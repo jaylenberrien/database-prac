0871185b476337ef3612a2efdda6da74b5ff3c5a
jaylenberrien <jaylenberrien.jb@gmail.com>
2025-02-06 22:26:13 -0500

    init

---
+++ client/index.js
@@ -0,0 +1,15 @@
+function logInput(){
+    const input = document.getElementById("main-input").value
+    alert(input)
+}
+
+//make a seperate section for the db queries that can be scrolled through
+//giving that its own container, adding some styling
+
+//work on connecting to db
+//work on downloading and using dapper for db work
+
+//working on model 
+//working on routes
+//working on controllers
+
