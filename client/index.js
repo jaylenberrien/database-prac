0428d3d21d9d630af52743c74eee1b7827a126c6
jaylenberrien <jaylenberrien.jb@gmail.com>
2025-02-17 07:55:18 -0500

    notes

--- client/index.js
+++ client/index.js
@@ -23,3 +23,11 @@ function deleteEntry(){
 //working on routes
 //working on controllers
 
+
+
+//we are going to make a csproj for the client so we can add packages that we need from docs
+//we are going to work getting the packages that we need for the controllers to work for the backend
+
+//i might need t omake a csproj that covers the scope of the entire project to make sure it is working properly. I will see
+
+//good work
